707937bb402035e057d31be813192116ab3a3311
Ronny Freites <ronnyangelo.freites@gmail.com>
2024-08-06 12:29:55 -0600

    add sitemap xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 // import node from "@astrojs/node";
 import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
+  site: import.meta.env.SITE_URL,
+  integrations: [sitemap()],
 });

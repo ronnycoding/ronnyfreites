10019a626dddca9e15c462422f8916316052dd0d
Ronny Freites <ronnyangelo.freites@gmail.com>
2024-09-21 11:32:04 -0600

    add code hightlighter support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
@@ -7,11 +6,10 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ronnyfreites.com',
-    // site: 'http://localhost:4321',
+    // site: 'https://ronnyfreites.com',
+    site: 'http://localhost:4321',
 
     integrations: [
-        mdx(),
         sitemap(),
         tailwind({
             applyBaseStyles: false

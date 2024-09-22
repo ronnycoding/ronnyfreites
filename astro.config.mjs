90091a90c127f7b13378de0bf1eb488cfc76790a
Ronny Freites <ronnyangelo.freites@gmail.com>
2024-09-21 20:21:59 -0600

    remove party town and GTM

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,16 @@ import tailwind from '@astrojs/tailwind';
 
 import vercel from '@astrojs/vercel/serverless';
 
-import partytown from '@astrojs/partytown';
-
 // https://astro.build/config
 export default defineConfig({
     site: process.env.VERCEL_ENV === 'production' ? 'https://ronnyfreites.com' : 'http://localhost:4321',
 
-    integrations: [sitemap(), tailwind({
-        applyBaseStyles: false
-    }), partytown()],
+    integrations: [
+        sitemap(),
+        tailwind({
+            applyBaseStyles: false
+        })
+    ],
 
     output: 'server',
     adapter: vercel()

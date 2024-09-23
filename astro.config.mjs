d437c5b5905c09ddd3c078e34781f8d1e540538a
Ronny Freites <ronnyangelo.freites@gmail.com>
2024-09-23 07:32:08 -0600

    add back party town

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 import vercel from '@astrojs/vercel/serverless';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
     site: process.env.VERCEL_ENV === 'production' ? 'https://ronnyfreites.com' : 'http://localhost:4321',
@@ -12,6 +14,10 @@ export default defineConfig({
         sitemap(),
         tailwind({
             applyBaseStyles: false
+        }),
+        partytown({
+            forward: ['dataLayer.push'],
+            debug: process.env.VERCEL_ENV !== 'production'
         })
     ],
 

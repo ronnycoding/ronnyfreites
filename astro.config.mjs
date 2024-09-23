9e5754240c5f405eaafccc2f69298fbe27b505c5
Ronny Freites <ronnyangelo.freites@gmail.com>
2024-09-22 20:23:39 -0600

    only GTM

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 import vercel from '@astrojs/vercel/serverless';
 
-import partytown from '@astrojs/partytown';
-
 // https://astro.build/config
 export default defineConfig({
     site: process.env.VERCEL_ENV === 'production' ? 'https://ronnyfreites.com' : 'http://localhost:4321',
@@ -14,8 +12,7 @@ export default defineConfig({
         sitemap(),
         tailwind({
             applyBaseStyles: false
-        }),
-        partytown({ config: { forward: ['dataLayer.push'] } })
+        })
     ],
 
     output: 'server',
